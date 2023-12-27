17275106ef1dca6c3514640ea348eaf6bf270c07
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-12-27 14:40:09 +0900

    Add: DB 연결 테스트 기능 추가

--- src/main/java/com/example/controller/ExRestController.java
+++ src/main/java/com/example/controller/ExRestController.java
@@ -1,5 +1,18 @@
 package com.example.controller;
 
-public class ExRestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dao.ExDAO;
+
+@RestController
+public class ExRestController {
+	@Autowired
+	ExDAO dao;
+	
+	@GetMapping("/now")
+	public String now() {
+		return dao.now();
+	}
 }
