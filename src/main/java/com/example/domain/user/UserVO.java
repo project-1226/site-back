330bdfd21b7f1b3aef9d30262da7fe1e4a3e0dc5
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-01 20:47:33 +0900

    Feat: User 회원가입, 로그인 기능 구현

---
+++ src/main/java/com/example/domain/user/UserVO.java
@@ -0,0 +1,76 @@
+package com.example.domain.user;
+
+import java.util.Date;
+
+public class UserVO {
+	private String email;
+	private String password;
+	private String nickname;
+	private String phone;
+	private String avatar;
+	private int point;
+	private Date regdate;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return "UserVO [email=" + email + ", password=" + password + ", nickname=" + nickname + ", phone=" + phone
+				+ ", avatar=" + avatar + ", point=" + point + ", regdate=" + regdate + "]";
+	}
+
+}
