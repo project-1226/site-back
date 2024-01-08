c5fef8239ce9ef3b54c7f8d578de336e10510451
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-08 18:31:57 +0900

    WIP: 상품 리뷰 관련 기능 구현

--- src/main/java/com/example/user/dao/ProductReviewDAOImpl.java
+++ src/main/java/com/example/user/dao/ProductReviewDAOImpl.java
@@ -1,5 +1,8 @@
 package com.example.user.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,5 +19,23 @@ public class ProductReviewDAOImpl implements ProductReviewDAO {
 	@Override
 	public void insert(ProductReviewVO vo) {
 		session.insert(namespace + ".insert", vo);
+	}
+
+	@Override
+	public List<HashMap<String, Object>> list(ProductReviewVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int total(ProductReviewVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<HashMap<String, Object>> image(ProductReviewVO vo) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
