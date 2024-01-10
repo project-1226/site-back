269665dd56e1fae3029ba66ccf982b22c3a2fad0
SongAhreum <thddkfmathd@naver.com>
2024-01-10 10:25:40 +0900

    WIP:건강식단 페이지 구현중 - 무작위로 카테고리별 category&food 뿌리기

--- src/test/java/com/example/BackApplicationTests.java
+++ src/test/java/com/example/BackApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.api.APIDAO;
+import com.example.api.APIDAOImpl;
+import com.example.food.dao.FoodDAO;
+import com.example.food.dao.FoodDAOImpl;
 
 @SpringBootTest
 class BackApplicationTests {
+	
+	@Autowired
+	APIDAO repository = new APIDAOImpl();
+	@Autowired
+	FoodDAO fooddao = new FoodDAOImpl();
 
+//	@Test
+//	void contextLoads() {
+//	}
+//	
+	
+//	@Test
+//	void findByCategory() {
+//		APIQueryVO vo = new APIQueryVO();
+//		vo.setName("폐암질환 식단");
+//		vo.setRegdate("2024-01-08");
+//		repository.category(vo);
+//	}
+//	@Test
+//	void findByFood() {
+//		APIQueryVO vo = new APIQueryVO();
+//		vo.setName("돼지고기사과구이");
+//		vo.setRegdate("2024-01-08");
+//		repository.food(vo);
+//	}
+	
 	@Test
-	void contextLoads() {
+	void categoryList() {
+		fooddao.categoryList("health");
 	}
 
 }
