10b42c936a12f6dac547fdca1e2ed17c2f483a30
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-17 06:36:38 +0900

    Feat: 문의 내역 기능 추가

--- src/main/java/com/example/user/controller/ShoppingQnaRestController.java
+++ src/main/java/com/example/user/controller/ShoppingQnaRestController.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.user.dao.ShoppingQnaDAO;
+import com.example.user.domain.ShoppingQnaVO;
 
 @RestController
 @RequestMapping("/shoppingqna")
@@ -17,8 +20,26 @@ public class ShoppingQnaRestController {
 	@Autowired
 	ShoppingQnaDAO dao;
 	
+	@GetMapping("/faq/list")
+	public List<HashMap<String, Object>> listFaq(@RequestParam("categoryid") int categoryid) {
+		return dao.listFaq(categoryid);
+	}
+	
+	@PostMapping("/insert")
+	public void insertQna(@RequestBody ShoppingQnaVO vo) {
+		dao.insertQna(vo);
+	}
+	
 	@GetMapping("/list")
-	public List<HashMap<String, Object>> list(@RequestParam("categoryid") int categoryid) {
-		return dao.list(categoryid);
+	public HashMap<String, Object> listQna(@RequestParam("userid") String userid) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("list", dao.listQna(userid));
+		map.put("total", dao.totalQna(userid));
+		return map;
+	}
+	
+	@PostMapping("/delete")
+	public void deleteQna(@RequestBody ShoppingQnaVO vo) {
+		dao.deleteQna(vo);
 	}
 }
--- src/main/java/com/example/user/dao/ShoppingQnaDAOImpl.java
+++ src/main/java/com/example/user/dao/ShoppingQnaDAOImpl.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.example.user.domain.ShoppingQnaVO;
+
 @Repository
 public class ShoppingQnaDAOImpl implements ShoppingQnaDAO {
 	@Autowired
@@ -15,9 +17,27 @@ public class ShoppingQnaDAOImpl implements ShoppingQnaDAO {
 	String namespace="com.example.mapper.ShoppingQnaMapper";
 
 	@Override
-	public List<HashMap<String, Object>> list(int categoryid) {
-		return session.selectList(namespace + ".list", categoryid);
+	public List<HashMap<String, Object>> listFaq(int categoryid) {
+		return session.selectList(namespace + ".list_faq", categoryid);
 	}
-	
-	
+
+	@Override
+	public void insertQna(ShoppingQnaVO vo) {
+		session.insert(namespace + ".insert_qna", vo);
+	}
+
+	@Override
+	public List<HashMap<String, Object>> listQna(String userid) {
+		return session.selectList(namespace + ".list_qna", userid);
+	}
+
+	@Override
+	public int totalQna(String userid) {
+		return session.selectOne(namespace + ".total_qna", userid);
+	}
+
+	@Override
+	public void deleteQna(ShoppingQnaVO vo) {
+		session.delete(namespace + ".delete_qna", vo);
+	}
 }
