87fc483dcd84ca6dc05e293401c5cfa884a59f63
sori-k <vrti0913@naver.com>
2024-01-21 20:34:09 +0900

    Feat: 관리자페이지 후기/댓글 페이지 구현

--- src/main/java/com/example/community_admin/dao/AdminDAO.java
+++ src/main/java/com/example/community_admin/dao/AdminDAO.java
@@ -3,6 +3,7 @@ package com.example.community_admin.dao;
 import java.util.HashMap;
 import java.util.List;
 
+import com.example.community_admin.domain.CommentVO;
 import com.example.community_admin.domain.PostVO;
 import com.example.community_admin.domain.ProductVO;
 import com.example.community_admin.domain.QueryVO;
@@ -25,4 +26,8 @@ public interface AdminDAO {
 	
 	public List<HashMap<String, Object>> imageList(int productid);
 	public void insertImage(ProductVO vo);
+	
+	public List<HashMap<String, Object>> qnaList(String userid);
+	public void insertQnaComment(CommentVO vo);
+	public List<HashMap<String, Object>> orderList();
 }
